1880d51319df178c35930e35934b4a4f2c5397fe
Mohammad Alkhatib <mohammad.alkhatib@web.de>
2024-11-23 22:00:48 +0100

    Removed comments.

--- extensions/micro-box2d/src/me/nulldoubt/micro/physics/box2d/JointDef.java
+++ extensions/micro-box2d/src/me/nulldoubt/micro/physics/box2d/JointDef.java
@@ -1,32 +1,41 @@
 package me.nulldoubt.micro.physics.box2d;
 
 public class JointDef {
+	
 	public enum JointType {
 		Unknown(0), RevoluteJoint(1), PrismaticJoint(2), DistanceJoint(3), PulleyJoint(4), MouseJoint(5), GearJoint(6), WheelJoint(
-			7), WeldJoint(8), FrictionJoint(9), RopeJoint(10), MotorJoint(11);
-
-		public static JointType[] valueTypes = new JointType[] {Unknown, RevoluteJoint, PrismaticJoint, DistanceJoint, PulleyJoint,
-			MouseJoint, GearJoint, WheelJoint, WeldJoint, FrictionJoint, RopeJoint, MotorJoint};
-		private int value;
-
-		JointType (int value) {
+				7), WeldJoint(8), FrictionJoint(9), RopeJoint(10), MotorJoint(11);
+		
+		public static final JointType[] valueTypes = new JointType[] {Unknown, RevoluteJoint, PrismaticJoint, DistanceJoint, PulleyJoint, MouseJoint, GearJoint, WheelJoint, WeldJoint, FrictionJoint, RopeJoint, MotorJoint};
+		private final int value;
+		
+		JointType(int value) {
 			this.value = value;
 		}
-
-		public int getValue () {
+		
+		public int getValue() {
 			return value;
 		}
 	}
-
-	/** The joint type is set automatically for concrete joint types. **/
+	
+	/**
+	 * The joint type is set automatically for concrete joint types.
+	 **/
 	public JointType type = JointType.Unknown;
-
-	/** The first attached body. **/
+	
+	/**
+	 * The first attached body.
+	 **/
 	public Body bodyA = null;
-
-	/** The second attached body **/
+	
+	/**
+	 * The second attached body
+	 **/
 	public Body bodyB = null;
-
-	/** Set this flag to true if the attached bodies should collide. **/
+	
+	/**
+	 * Set this flag to true if the attached bodies should collide.
+	 **/
 	public boolean collideConnected = false;
+	
 }
